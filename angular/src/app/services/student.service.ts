f94b520aa4e3a6d6fc6305eb19b08285b70d0ef6
Manaus Transez <manatran@student.arteveldehs.be>
2018-11-07 20:57:35 +0100

    Added student fetch

---
+++ angular/src/app/services/student.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Student } from '../models/student';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  private fetchURL = 'http://localhost/jsonapi/student/student';
+
+  constructor() {}
+
+  public async getStudents<T>(): Promise<T> {
+    try {
+      const res = await axios.request<T>({
+        method: 'get',
+        url: this.fetchURL
+      });
+      return res.data;
+    } catch (error) {
+      return Promise.reject(this.handleError(error));
+    }
+  }
+
+  public async getStudent<T>(id: string): Promise<T> {
+    try {
+      const res = await axios.request<T>({
+        method: 'get',
+        url: `${this.fetchURL}/${id}?include=field_courses`
+      });
+      return res.data;
+    } catch (error) {
+      return Promise.reject(this.handleError(error));
+    }
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+}
--- angular/src/app/students/students.component.ts
+++ angular/src/app/students/students.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from './../services/student.service';
+import { Student } from '../models/student';
+import { JsonObject } from '../models/json-object';
 
 @Component({
   selector: 'app-students',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+  public students: Student[];
 
-  constructor() { }
+  constructor(private studentService: StudentService) { }
 
   ngOnInit() {
+    this.getStudents();
+  }
+
+  public async getStudents(): Promise<void> {
+    try {
+      const res = await this.studentService.getStudents<JsonObject>();
+      this.students = res.data;
+      console.log(this.students);
+    } catch ( error ) {
+      console.error( error );
+    }
   }
 
 }
