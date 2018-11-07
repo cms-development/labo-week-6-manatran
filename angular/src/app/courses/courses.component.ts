e0a1fddac5d3b6cdbd17b81f32a16c3cb4533e93
Manaus Transez <manatran@student.arteveldehs.be>
2018-11-08 00:02:40 +0100

    Added instructors fetch

--- angular/src/app/courses/courses.component.ts
+++ angular/src/app/courses/courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from './../services/course.service';
+import { Course } from '../models/course';
+import { JsonObject } from '../models/json-object';
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+  public courses: Course[];
 
-  constructor() { }
+  constructor(private courseService: CourseService) { }
 
   ngOnInit() {
+    this.getCourses();
   }
 
+  public async getCourses(): Promise<void> {
+    try {
+      const res = await this.courseService.getCourses<JsonObject>();
+      this.courses = res.data;
+      console.log(this.courses);
+    } catch ( error ) {
+      console.error( error );
+    }
+  }
 }
